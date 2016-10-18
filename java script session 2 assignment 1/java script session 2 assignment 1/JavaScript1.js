fc7adcccb7ba733f3244a3932add91d3a0252ffd
praveenearle <praveen.earle@gmail.com>
2016-10-18 09:42:22 +0530

    java script s2a1
    java script s2a1

---
+++ java script session 2 assignment 1/java script session 2 assignment 1/JavaScript1.js
@@ -0,0 +1,6 @@
+﻿function minMax() {
+    document.getElementById("sachin").innerHTML = 'maximum number:' +Math.max(10, 20, 30);
+    document.getElementById("dravid").innerHTML = 'minimum number:' +Math.min(10, 20,30);
+
+
+}
---
+++ java script session 2 assignment 1/java script session 2 assignment 1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("java_script_session_2_assignment_1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("java_script_session_2_assignment_1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f1838c04-d7e4-4498-8b07-36940e5bc945")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
